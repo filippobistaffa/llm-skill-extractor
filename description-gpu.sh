2e04fc5e8a8cf85123c1cad9e2cdf74bd9eda135
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-11-10 14:40:59 +0100

    Minimum number of CPUs is 32

--- description-gpu.sh
+++ description-gpu.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --time=10:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=16G
 #SBATCH --output=description-gpu.out
 #SBATCH --error=description-gpu.err
