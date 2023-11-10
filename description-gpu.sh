1a44528f15886b15aca5dc150d07c165d61758e9
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-11-10 14:40:02 +0100

    Adjusted number of threads

--- description-gpu.sh
+++ description-gpu.sh
@@ -3,11 +3,11 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --time=10:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=16G
 #SBATCH --output=description-gpu.out
 #SBATCH --error=description-gpu.err
 
 module load python/3.9.9
 
-srun python3 description.py --threads 20 --n-gpu-layers 40
+srun python3 description.py --n-gpu-layers 40
