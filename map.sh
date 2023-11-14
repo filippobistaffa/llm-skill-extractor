636909aca91574f0e01b5e4883edf02186a4e4e2
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2023-11-14 19:51:30 +0100

    Fixed some names

--- map.sh
+++ map.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=llama-cpp-label
+#SBATCH --job-name=llama-cpp-map
 #SBATCH --time=30:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=64G
-#SBATCH --output=label.out
-#SBATCH --error=label.err
+#SBATCH --output=map.out
+#SBATCH --error=map.err
 
 if [ "$USER" == "filippo.bistaffa" ]
 then
