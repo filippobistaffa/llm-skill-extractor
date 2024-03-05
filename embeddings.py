08bdc06d558a04256a2223b26e00361f073c703c
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2024-03-05 21:31:33 +0100

    Updated

--- embeddings.py
+++ embeddings.py
@@ -1,3 +1,8 @@
+import argparse as ap
+import pandas as pd
+import numpy as np
+import os
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -6,11 +11,7 @@ client = OpenAI()
 
 def get_embedding(text, model='text-embedding-3-small'):
    text = text.replace('\n', ' ')
-   return client.embeddings.create(input=[text], model=model).data[0].embedding
-
-import argparse as ap
-import pandas as pd
-import os
+   return np.array(client.embeddings.create(input=[text], model=model).data[0].embedding)
 
 
 if __name__ == "__main__":
@@ -18,7 +19,7 @@ if __name__ == "__main__":
     parser = ap.ArgumentParser()
     parser.add_argument('--skills', type=str, default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'skills.csv'))
     parser.add_argument('--model', type=str, default='text-embedding-3-small', choices=['text-embedding-3-small', 'text-embedding-3-large'])
-    parser.add_argument('--embeddings', type=str, default='skills-embeddings.pkl')
+    parser.add_argument('--embeddings', type=str, default='skills-embeddings-3-small.tar.gz')
     args, additional = parser.parse_known_args()
 
     df = pd.read_csv(args.skills, index_col=0)
--- map.py
+++ map.py
@@ -4,13 +4,36 @@ import numpy as np
 import os
 
 
-if __name__ == "__main__":
+from dotenv import load_dotenv
+load_dotenv()
 
-    # parse command-line arguments
+from openai import OpenAI
+client = OpenAI()
+
+def get_embedding(text, model='text-embedding-3-small'):
+   text = text.replace('\n', ' ')
+   return np.array(client.embeddings.create(input=[text], model=model).data[0].embedding)
+
+
+def cosine_similarity(a, b):
+    #print(a)
+    #print(b)
+    cos_sim = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+    #print(cos_sim)
+    return cos_sim
+
+
+if __name__ == "__main__":
     parser = ap.ArgumentParser()
     parser.add_argument('--embeddings', type=str, default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'skills-embeddings-3-small.tar.gz'))
     parser.add_argument('--skills', type=str, default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'clean-0.csv'))
+    parser.add_argument('--model', type=str, default='text-embedding-3-small', choices=['text-embedding-3-small', 'text-embedding-3-large'])
+    parser.add_argument('-n', type=int, default=5)
     args, additional = parser.parse_known_args()
-
     embeddings = pd.read_pickle(args.embeddings)
-    print(embeddings)
+    input = pd.read_csv(args.skills, index_col=0, sep=';')
+    input['embedding'] = input['skill'].apply(lambda x: get_embedding(x, model=args.model))
+    for index, row in input.iterrows():
+        embeddings['cos_sim'] = embeddings[f'embedding'].apply(lambda x: cosine_similarity(x, row['embedding']))
+        print(row['skill'])
+        print(embeddings.nlargest(args.n, 'cos_sim'))
