f7beb547e20302857bc0cf9794009b8ddee5a2dd
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2024-03-05 23:08:46 +0100

    Save output

--- map.py
+++ map.py
@@ -28,12 +28,19 @@ if __name__ == "__main__":
     parser.add_argument('--embeddings', type=str, default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'skills-embeddings-3-small.tar.gz'))
     parser.add_argument('--skills', type=str, default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'clean-0.csv'))
     parser.add_argument('--model', type=str, default='text-embedding-3-small', choices=['text-embedding-3-small', 'text-embedding-3-large'])
-    parser.add_argument('-n', type=int, default=5)
+    parser.add_argument('-n', type=int, default=3)
+    parser.add_argument('--output', type=str)
     args, additional = parser.parse_known_args()
     embeddings = pd.read_pickle(args.embeddings)
     input = pd.read_csv(args.skills, index_col=0, sep=';')
     input['embedding'] = input['skill'].apply(lambda x: get_embedding(x, model=args.model))
-    for index, row in input.iterrows():
+    output = []
+    for i, row in input.iterrows():
         embeddings['cos_sim'] = embeddings[f'embedding'].apply(lambda x: cosine_similarity(x, row['embedding']))
+        topn = embeddings.nlargest(args.n, 'cos_sim')
         print(row['skill'])
-        print(embeddings.nlargest(args.n, 'cos_sim'))
+        print(topn)
+        for _, row_top in topn.iterrows():
+            output.append([i + 1, row['skill'], row_top['skill'], row_top['cos_sim']])
+    if args.output is not None:
+        pd.DataFrame(output, columns=['id', 'skill', 'mapping', 'value']).to_csv(args.output, sep=';', index=False)
