1d9d8da060fb118df4d1e9b1ee4f9a3a08f3796a
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2024-03-06 11:18:45 +0100

    Cosmetics

--- embeddings.py
+++ embeddings.py
@@ -15,13 +15,11 @@ def get_embedding(text, model='text-embedding-3-small'):
 
 
 if __name__ == "__main__":
-
     parser = ap.ArgumentParser()
     parser.add_argument('--skills', type=str, default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'skills.csv'))
     parser.add_argument('--model', type=str, default='text-embedding-3-small', choices=['text-embedding-3-small', 'text-embedding-3-large'])
     parser.add_argument('--embeddings', type=str, default='skills-embeddings-3-small.tar.gz')
     args, additional = parser.parse_known_args()
-
     df = pd.read_csv(args.skills, index_col=0)
     df['embedding'] = df['skill'].apply(lambda x: get_embedding(x, model=args.model))
     df.to_pickle(args.embeddings)
