b76ca9f949a2e363b8444badecdcb261c9702345
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2024-03-26 18:06:36 +0100

    Use `llama-cpp-python`

--- description.py
+++ description.py
@@ -1,22 +1,21 @@
 import argparse as ap
 import pandas as pd
 import numpy as np
-import subprocess
-import sys
+import llama_cpp
 import os
 
 
 if __name__ == "__main__":
 
-    llama_cpp_subdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'llama.cpp')
-
     # parse command-line arguments
     parser = ap.ArgumentParser()
     parser.add_argument('--skills_dataset', type=str, default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'skills.txt'))
     parser.add_argument('--n_skills', type=int, default=3)
-    parser.add_argument('--model', type=str, default=os.path.join(llama_cpp_subdir, 'models', 'mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf'))
-    parser.add_argument('--format', type=str, default='[INST] {} [/INST]')
-    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--model', type=str, default=os.path.join('models', 'mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf'))
+    parser.add_argument('--seed', type=int, default=llama_cpp.LLAMA_DEFAULT_SEED)
+    parser.add_argument('--ctx', type=int, default=2048)
+    parser.add_argument('--threads', type=int)
+    parser.add_argument('--verbose', action='store_true')
     args, additional = parser.parse_known_args()
 
     # build prompt
@@ -24,32 +23,21 @@ if __name__ == "__main__":
     skills_list = pd.read_csv(args.skills_dataset, sep='\t', header=None).values.ravel()
     skills_sample = np.random.choice(skills_list, size=args.n_skills)
     skills_string = ', '.join(skills_sample[:-1]).lower() + ', and ' + skills_sample[-1].lower()
-    prompt = f'Give me the description of a professional course to learn how to {skills_string}'
-    prompt_format = args.format.format(prompt)
-    
-    # llama.cpp parameters
-    llama_cpp_params = {
-        '--model': args.model,
-        '--ctx-size': str(len(prompt_format)),
-        '--seed': str(args.seed),
-        '--prompt': prompt_format,
-        '--repeat_penalty': '1.1',
-        '--n-predict': '-1',
-        '--temp': '0.7',
-    }
+    prompt = f'Give me the description of a professional course to learn how to {skills_string}.'
 
-    # build subprocess (llama.cpp) command-line
-    command_line = [os.path.join(llama_cpp_subdir, 'build', 'bin' ,'main'), '--escape', '--log-disable']
-    for (param, value) in llama_cpp_params.items():
-        command_line.extend([param, value])
-    command_line.extend(additional) # by putting additional at the end we can override the default ones
+    # build LLM
+    llm = llama_cpp.Llama(
+        model_path = args.model,
+        n_threads = args.threads,
+        seed = args.seed,
+        n_ctx = args.ctx,
+        verbose = args.verbose,
+    )
 
-    #print(command_line)
-    #quit()
+    output = llm(
+        prompt,
+        max_tokens = -1,
+        stop = ['</s>'],
+    )
 
-    # run subprocess
-    result = subprocess.run(command_line, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    # print stdout and stderr
-    print(result.stdout.decode().rstrip())
-    print(result.stderr.decode().rstrip(), file=sys.stderr)
+    print(output['choices'][0]['text'])
