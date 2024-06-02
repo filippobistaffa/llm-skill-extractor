5bdc18dfa26146a091c4630fbccb17900f0a8b47
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2024-06-02 20:09:58 +0200

    Support for Mistral AI

--- llm-embeddings.py
+++ llm-embeddings.py
@@ -3,16 +3,17 @@ import pandas as pd
 import numpy as np
 import json
 import os
+import re
 
 from dotenv import load_dotenv
 load_dotenv()
 
 from openai import OpenAI
-client = OpenAI()
+oaclient = OpenAI()
 
 def get_embedding(text, model='text-embedding-3-small'):
    text = text.replace('\n', ' ')
-   return np.array(client.embeddings.create(input=[text], model=model).data[0].embedding)
+   return np.array(oaclient.embeddings.create(input=[text], model=model).data[0].embedding)
 
 def cosine_similarity(a, b):
     if len(a) != len(b):
@@ -24,7 +25,17 @@ def cosine_similarity(a, b):
 if __name__ == '__main__':
     parser = ap.ArgumentParser()
     parser.add_argument('--description', type=str, default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'description.txt'))
-    parser.add_argument('--llm', type=str, default='gpt-3.5-turbo', choices=['gpt-3.5-turbo', 'gpt-4o'])
+    parser.add_argument('--llm', type=str, default='gpt-3.5-turbo', choices=[
+        # OpenAI
+        'gpt-3.5-turbo',
+        'gpt-4o',
+        # Mistral AI
+        'open-mistral-7b',
+        'open-mixtral-8x7b',
+        'open-mixtral-8x22b',
+        'mistral-small-latest',
+        'mistral-large-latest',
+    ])
     parser.add_argument('--embeddings', type=str, default='small', choices=['small', 'large'])
     parser.add_argument('--temperature', type=float, default=0) # should be 0 <= temperature <= 2
     parser.add_argument('-n', type=int, default=5)
@@ -37,20 +48,36 @@ if __name__ == '__main__':
         description = f'' + ''.join(f.readlines()).strip()
     prompt = f'Output a list of {args.n} specialist tasks taken from the Australian Skill Framework that are related to the following course:\n{description}'
 
-    # refer to https://platform.openai.com/docs/api-reference/chat
-    response = client.chat.completions.create(
-        model=args.llm,
-        temperature=args.temperature,
-        messages=[
-            {'role': 'user', 'content': prompt},
-            {'role': 'system', 'content': 'Answer only with a numbered list of skills, nothing else.'},
-        ]
-    ).choices[0].message.content
+    if args.llm.startswith('gpt'):
+        # refer to https://platform.openai.com/docs/api-reference/chat
+        response = oaclient.chat.completions.create(
+            model=args.llm,
+            temperature=args.temperature,
+            messages=[
+                {'role': 'user', 'content': prompt},
+                {'role': 'system', 'content': 'Answer only with a numbered list of skills, nothing else.'},
+            ]
+        ).choices[0].message.content
+    else:
+        from mistralai.client import MistralClient
+        from mistralai.models.chat_completion import ChatMessage
+        # refer to https://docs.mistral.ai/api/
+        client = MistralClient(api_key=os.environ['MISTRAL_API_KEY'])
+        response = client.chat(
+            model=args.llm,
+            temperature=args.temperature,
+            messages=[
+                ChatMessage(role='user', content=prompt),
+                ChatMessage(role='system', content='Answer only with a numbered list of skills, nothing else.')
+            ]
+        ).choices[0].message.content
 
-    print(f'LLM ({args.llm}) response:\n{response}')
+    # keep only lines of the numbered list
+    response = [line.split('. ', 1)[1].strip() for line in response.splitlines() if re.match(r'^\d+\. ', line)]
+    print(f'LLM ({args.llm}) response:\n- {"\n- ".join(response)}')
 
     # compute embeddings for the labels produced by the LLM
-    labels = pd.DataFrame([line.split('. ', 1)[1] for line in response.splitlines()], columns=['label'])
+    labels = pd.DataFrame(response, columns=['label'])
     labels['embedding'] = labels['label'].apply(lambda x: get_embedding(x, model=f'text-embedding-3-{args.embeddings}'))
 
     # load precomputed embeddings of the skills in the framework
